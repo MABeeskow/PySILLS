18b953bc7d5a43da4c4a2b4dc448e310c30b81b1
Maximilian Beeskow <maximilian.beeskow@rwth-aachen.de>
2023-08-01 22:39:10 +0200

    "extended data_reduction.py"

--- modules/data_reduction.py
+++ modules/data_reduction.py
@@ -5,28 +5,45 @@
 
 # Name:		data_reduction.py
 # Author:	Maximilian A. Beeskow
-# Version:	1.0
-# Date:		24.07.2023
+# Version:	pre-release
+# Date:		01.08.2023
 
 #-----------------------------------------------------------------------------------------------------------------------
 
 ## MODULES -------------------------------------------------------------------------------------------------------------
-import numpy as np
+import pandas as pd
 
 ## CLASSES -------------------------------------------------------------------------------------------------------------
 class DataExtraction:
-    def __init__(self):
-        pass
+    def __init__(self, filename_long):
+        self.filename_long = filename_long
 
-    def get_measurements(self):
-        pass
+    def get_measurements(self, delimiter, skip_header, skip_footer):
+        self.delimiter = delimiter
+        self.skip_header = skip_header
+        self.skip_footer = skip_footer
 
-    def get_isotopes(self):
-        pass
+        df_complete = pd.read_csv(
+            self.filename_long, sep=self.delimiter, header=self.skip_header, skipfooter=self.skip_footer,
+            engine="python")
+
+        return df_complete
+
+    def get_isotopes(self, dataframe):
+        list_names = list(dataframe.columns.values)
+        list_names.pop(0)
+        list_isotopes = list_names
+
+        return list_isotopes
+
+    def get_times(self, dataframe):
+        df_times = dataframe.iloc[:, 0]
+
+        return df_times
 
 class IntensityQuantification:
-    def __init__(self):
-        pass
+    def __init__(self, internal_standard):
+        self.internal_standard = internal_standard
 
     def get_intensity(self):
         # Background
@@ -36,12 +53,12 @@ class IntensityQuantification:
         # Inclusion Signal
         pass
 
-    def get_intensity_ratio(self):
+    def get_intensity_ratio(self, internal_standard):
         pass
 
 class SensitivityQuantification:
-    def __init__(self):
-        pass
+    def __init__(self, internal_standard):
+        self.internal_standard = internal_standard
 
     def get_analytical_sensitivity(self):
         pass
@@ -53,8 +70,8 @@ class SensitivityQuantification:
         pass
 
 class CompositionQuantification:
-    def __init__(self):
-        pass
+    def __init__(self, internal_standard):
+        self.internal_standard = internal_standard
 
     def get_composition(self):
         # Mineral Analysis / Matrix Analysis
