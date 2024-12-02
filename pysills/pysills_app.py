c7a0b4872a59749f50dada429cdfb264777ac163
Maximilian Beeskow <maximilian.beeskow@rwth-aachen.de>
2024-12-02 20:39:55 +0100

    "bug fixing, code cleaning and quality-of-life improvements"

--- pysills/pysills_app.py
+++ pysills/pysills_app.py
@@ -5,7 +5,7 @@
 
 # Name:		pysills_app.py
 # Author:	Maximilian A. Beeskow
-# Version:	v1.0.43
+# Version:	v1.0.44
 # Date:		02.12.2024
 
 # -----------------------------------------------------------------------------------------------------------------------
@@ -72,7 +72,7 @@ class PySILLS(tk.Frame):
             var_scaling = 1.3
 
         ## Current version
-        self.str_version_number = "1.0.43"
+        self.str_version_number = "1.0.44"
         self.val_version = self.str_version_number + " - 02.12.2024"
 
         ## Colors
