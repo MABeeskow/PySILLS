cdbd05658b4715a8d349baa5707e8da73e5ec784
MABeeskow <maximilian.beeskow@rwth-aachen.de>
2026-04-12 19:08:55 +0200

    "adjusted the new GUI"

--- src/pysills/gui/pysills_main.py
+++ src/pysills/gui/pysills_main.py
@@ -5,7 +5,7 @@
 # Name:        pysills_main.py
 # Author:      Maximilian A. Beeskow
 # Version:     v1.0.0 (GUI prototype)
-# Date:        31.03.2026
+# Date:        12.04.2026
 # ----------------------------------------------------------------------------------------------------------------------
 
 # MODULES
@@ -21,20 +21,21 @@ class PySILLSApp:
         self.root = root
         self.skin = skin
         self.root.title("PySILLS")
-        self.root.geometry("1250x750")
+        self.root.geometry("1200x800")
 
         # --- Main container ---
         self.main_frame = ttk.Frame(self.root, padding=10)
         self.main_frame.pack(fill="both", expand=True)
 
         # Layout konfigurieren
-        self.main_frame.columnconfigure(0, weight=1)
-        self.main_frame.columnconfigure(1, weight=2)
-        self.main_frame.columnconfigure(2, weight=3)
-        self.main_frame.columnconfigure(3, weight=3)
-        self.main_frame.rowconfigure(0, weight=1)
-        self.main_frame.rowconfigure(1, weight=3)
-        self.main_frame.rowconfigure(2, weight=1)
+        self.main_frame.columnconfigure(0, weight=0, minsize=250)
+        self.main_frame.columnconfigure(1, weight=1, uniform="content")
+        self.main_frame.columnconfigure(2, weight=1, uniform="content")
+        self.main_frame.columnconfigure(3, weight=1, uniform="content")
+
+        self.main_frame.rowconfigure(0, weight=0, minsize=300)
+        self.main_frame.rowconfigure(1, weight=1, minsize=320)
+        self.main_frame.rowconfigure(2, weight=0)
 
         # UI initialisieren
         self.create_widgets()
@@ -53,27 +54,30 @@ class PySILLSApp:
                 button.state(["!pressed"])
 
     def create_widgets(self):
+        # Settings
+        padding_frame = 10
+        padding_button = 2
 
         # 1st panel (PySILLS)
-        self.frame_main = ttk.LabelFrame(self.main_frame, text="General", padding=10)
-        self.frame_main.grid(row=1, column=0, sticky="nsew", padx=(0, 10), pady=(0, 10))
+        self.frame_main = ttk.LabelFrame(self.main_frame, text="General", padding=padding_frame)
+        self.frame_main.grid(row=1, column=0, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         # Buttons
         self.btn_frame_main = ttk.Frame(self.frame_main)
-        self.btn_frame_main.pack(fill="x", pady=5)
+        self.btn_frame_main.pack(fill="x", pady=padding_frame/2)
 
-        ttk.Button(self.btn_frame_main, text="New project").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_main, text="Open project").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_main, text="Save project").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_main, text="Close project").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_main, text="Settings").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_main, text="Documentation").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_main, text="About").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_main, text="Quit").pack(fill="x", pady=2)
+        ttk.Button(self.btn_frame_main, text="New project").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_main, text="Open project").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_main, text="Save project").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_main, text="Close project").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_main, text="Settings").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_main, text="Documentation").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_main, text="About").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_main, text="Quit").pack(fill="x", pady=padding_button)
 
         # 2nd panel (Project setup)
-        self.frame_project = ttk.LabelFrame(self.main_frame, text="Project settings", padding=10)
-        self.frame_project.grid(row=0, column=0, sticky="nsew", padx=(0, 10), pady=(0, 10))
+        self.frame_project = ttk.LabelFrame(self.main_frame, text="Project settings", padding=padding_frame)
+        self.frame_project.grid(row=0, column=0, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         # Toggle-Buttons
         self.radio_var = tk.StringVar(value="option1")
@@ -86,38 +90,51 @@ class PySILLSApp:
         for text, value in options:
             btn = ttk.Button(
                 self.frame_project, text=text, style="Toggle.TButton", command=lambda v=value: self.set_mode(v))
-            btn.pack(fill="x", pady=2)
+            btn.pack(fill="x", pady=padding_button)
             self.toggle_buttons[value] = btn
 
         self.update_toggle_styles()
 
         # Buttons
         self.btn_frame_project = ttk.Frame(self.frame_project)
-        self.btn_frame_project.pack(fill="x", pady=5)
+        self.btn_frame_project.pack(fill="x", pady=padding_frame/2)
 
-        ttk.Button(self.btn_frame_project, text="Setup").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_project, text="Results").pack(fill="x", pady=2)
+        ttk.Button(self.btn_frame_project, text="Setup").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_project, text="Results").pack(fill="x", pady=padding_button)
 
         # 3rd panel (Standard files)
         self.frame_standard = ttk.LabelFrame(
-            self.main_frame, text="Standard reference material (SRM) settings", padding=10)
-        self.frame_standard.grid(row=0, column=1, sticky="nsew", padx=(0, 10), pady=(0, 10))
+            self.main_frame, text="Standard reference material (SRM) settings", padding=padding_frame)
+        self.frame_standard.grid(row=0, column=1, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         # Buttons
         self.btn_frame_standard = ttk.Frame(self.frame_standard)
-        self.btn_frame_standard.pack(fill="x", pady=5)
+        self.btn_frame_standard.pack(fill="x", pady=padding_frame/2)
 
-        ttk.Button(self.btn_frame_standard, text="Add file").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_standard, text="Copy file").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_standard, text="Remove file").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_standard, text="View file data").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_standard, text="Interval setup").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_standard, text="Spike elimination").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_standard, text="View file results").pack(fill="x", pady=2)
+        buttons_std = [
+            "Add file", "Copy file", "Remove file", "View file data", "Interval setup", "Spike elimination",
+            "View file results"]
+        for i, text in enumerate(buttons_std):
+            if i < 4:
+                r = i//2
+                c = i%2
+                col_span = 1
+            else:
+                r = i - 2
+                c = 0
+                col_span = 2
+
+            ttk.Button(self.btn_frame_standard, text=text).grid(
+                row=r, column=c, columnspan=col_span, sticky="ew", padx=padding_button, pady=padding_button,
+                ipady=padding_button)
+
+        self.btn_frame_standard.columnconfigure(0, weight=1)
+        self.btn_frame_standard.columnconfigure(1, weight=1)
 
         # Listbox (z.B. für Messfiles)
-        self.frame_listbox_standard = ttk.LabelFrame(self.main_frame, text="Imported files", padding=10)
-        self.frame_listbox_standard.grid(row=1, column=1, sticky="nsew", padx=(0, 10), pady=(0, 10))
+        self.frame_listbox_standard = ttk.LabelFrame(self.main_frame, text="Imported files", padding=padding_frame)
+        self.frame_listbox_standard.grid(
+            row=1, column=1, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         self.file_list_standard = tk.Listbox(self.frame_listbox_standard, height=20)
         self.file_list_standard.pack(fill="both", expand=True)
@@ -127,24 +144,36 @@ class PySILLSApp:
             self.file_list_standard.insert(tk.END, f"Standard_File_{i}.csv")
 
         # 4th panel (Sample files)
-        self.frame_sample = ttk.LabelFrame(self.main_frame, text="Sample files settings", padding=10)
-        self.frame_sample.grid(row=0, column=2, sticky="nsew", padx=(0, 10), pady=(0, 10))
+        self.frame_sample = ttk.LabelFrame(self.main_frame, text="Sample files settings", padding=padding_frame)
+        self.frame_sample.grid(row=0, column=2, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         # Buttons
         self.btn_frame_sample = ttk.Frame(self.frame_sample)
-        self.btn_frame_sample.pack(fill="x", pady=5)
+        self.btn_frame_sample.pack(fill="x", pady=padding_frame/2)
 
-        ttk.Button(self.btn_frame_sample, text="Add file").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_sample, text="Copy file").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_sample, text="Remove file").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_sample, text="View file data").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_sample, text="Interval setup").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_sample, text="Spike elimination").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_sample, text="View file results").pack(fill="x", pady=2)
+        buttons_smpl = [
+            "Add file", "Copy file", "Remove file", "View file data", "Interval setup", "Spike elimination",
+            "View file results"]
+        for i, text in enumerate(buttons_smpl):
+            if i < 4:
+                r = i//2
+                c = i%2
+                col_span = 1
+            else:
+                r = i - 2
+                c = 0
+                col_span = 2
+
+            ttk.Button(self.btn_frame_sample, text=text).grid(
+                row=r, column=c, columnspan=col_span, sticky="ew", padx=padding_button, pady=padding_button,
+                ipady=padding_button)
+
+        self.btn_frame_sample.columnconfigure(0, weight=1)
+        self.btn_frame_sample.columnconfigure(1, weight=1)
 
         # Listbox (z.B. für Messfiles)
-        self.frame_listbox_sample = ttk.LabelFrame(self.main_frame, text="Imported files", padding=10)
-        self.frame_listbox_sample.grid(row=1, column=2, sticky="nsew", padx=(0, 10), pady=(0, 10))
+        self.frame_listbox_sample = ttk.LabelFrame(self.main_frame, text="Imported files", padding=padding_frame)
+        self.frame_listbox_sample.grid(row=1, column=2, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         self.file_list_sample = tk.Listbox(self.frame_listbox_sample, height=20)
         self.file_list_sample.pack(fill="both", expand=True)
@@ -154,20 +183,21 @@ class PySILLSApp:
             self.file_list_sample.insert(tk.END, f"Sample_File_{i}.csv")
 
         # 5th panel (Isotopes)
-        self.frame_isotopes = ttk.LabelFrame(self.main_frame, text="Isotope settings", padding=10)
-        self.frame_isotopes.grid(row=0, column=3, sticky="nsew", padx=(0, 10), pady=(0, 10))
+        self.frame_isotopes = ttk.LabelFrame(self.main_frame, text="Isotope settings", padding=padding_frame)
+        self.frame_isotopes.grid(row=0, column=3, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         # Buttons
         self.btn_frame_isotopes = ttk.Frame(self.frame_isotopes)
-        self.btn_frame_isotopes.pack(fill="x", pady=5)
+        self.btn_frame_isotopes.pack(fill="x", pady=padding_frame/2)
 
-        ttk.Button(self.btn_frame_isotopes, text="Assign SRM").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_isotopes, text="View SRM values").pack(fill="x", pady=2)
-        ttk.Button(self.btn_frame_isotopes, text="View isotope results").pack(fill="x", pady=2)
+        ttk.Button(self.btn_frame_isotopes, text="Assign SRM").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_isotopes, text="View SRM values").pack(fill="x", pady=padding_button)
+        ttk.Button(self.btn_frame_isotopes, text="View isotope results").pack(fill="x", pady=padding_button)
 
         # Listbox (z.B. für Messfiles)
-        self.frame_listbox_isotopes = ttk.LabelFrame(self.main_frame, text="Measured isotopes", padding=10)
-        self.frame_listbox_isotopes.grid(row=1, column=3, sticky="nsew", padx=(0, 10), pady=(0, 10))
+        self.frame_listbox_isotopes = ttk.LabelFrame(self.main_frame, text="Measured isotopes", padding=padding_frame)
+        self.frame_listbox_isotopes.grid(
+            row=1, column=3, sticky="nsew", padx=(0, padding_frame), pady=(0, padding_frame))
 
         self.list_isotopes = tk.Listbox(self.frame_listbox_isotopes, height=20)
         self.list_isotopes.pack(fill="both", expand=True)
@@ -182,7 +212,7 @@ class PySILLSApp:
         style.configure("Toggle.TButton",
             relief="flat",
             borderwidth=0,
-            padding=6
+            padding=padding_frame/2
         )
         if self.skin == "dark":
             listbox_style = {
@@ -290,7 +320,7 @@ def configure_light_theme(root):
                 "configure": {
                     "background": "#d5d5d5",
                     "foreground": "#000000",
-                    "padding": 6,
+                    "padding": (5, 5),
                     "relief": "flat",
                     "borderwidth": 1
                 },
@@ -307,7 +337,10 @@ def configure_light_theme(root):
             "TRadiobutton": {
                 "configure": {
                     "background": "#e0e0e0",
-                    "foreground": "#000000"
+                    "foreground": "#000000",
+                    "padding": (5, 5),
+                    "relief": "flat",
+                    "borderwidth": 1
                 }
             },
             "TEntry": {
@@ -376,7 +409,7 @@ def configure_dark_theme(root):
                 "configure": {
                     "background": "#3a3a3a",
                     "foreground": "#ffffff",
-                    "padding": 6,
+                    "padding": (5, 5),
                     "relief": "flat",
                     "borderwidth": 1
                 },
@@ -394,7 +427,10 @@ def configure_dark_theme(root):
             "TRadiobutton": {
                 "configure": {
                     "background": "#2b2b2b",
-                    "foreground": "#e6e6e6"
+                    "foreground": "#e6e6e6",
+                    "padding": (5, 5),
+                    "relief": "flat",
+                    "borderwidth": 1
                 }
             },
 
